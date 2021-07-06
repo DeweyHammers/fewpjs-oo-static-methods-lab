f8ad0412b05e347fdb6c3a628295d6e999addc49
Dewey <hammersdrichard@gmail.com>
2021-07-06 12:49:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    str = string.replace(/[^a-z0-9áéíóúñü \.,_]/gim,"");
+    return str.trim();
+  }
 }
