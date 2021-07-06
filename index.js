8d0326b669d9c6a209402ade709d71352f9742b0
Dewey <hammersdrichard@gmail.com>
2021-07-06 13:26:03 -0700

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,52 @@ class Formatter {
   }
 
   static sanitize(string) {
-    str = string.replace(/[^a-z0-9áéíóúñü \.,_]/gim,"");
-    return str.trim();
+    return string.replace(/[^a-z0-9- \.',_]/gim,"");
+  }
+
+  static titleize(sentence) {
+    const newSentence = []
+    sentence.split(" ").forEach(word => {
+      switch (word) {
+        case 'the':
+          newSentence.push(word);
+          break;
+        case 'a':
+          if (sentence[0] === 'a'){
+            newSentence.push(word.toUpperCase());
+          } else {
+            newSentence.push(word);
+          }
+          break;
+        case 'an':
+          newSentence.push(word);
+          break;
+        case 'but':
+          newSentence.push(word);
+          break;
+        case 'of':
+          newSentence.push(word);
+          break;
+        case 'and':
+          newSentence.push(word);
+          break;
+        case 'for':
+          newSentence.push(word);
+          break;
+        case 'at':
+          newSentence.push(word);
+          break;
+        case 'by':
+          newSentence.push(word);
+          break;
+        case 'from': 
+          newSentence.push(word);
+          break;
+        default:
+          newSentence.push(word.charAt(0).toUpperCase() + word.slice(1));
+          break;
+      }
+    });
+    return newSentence.join(" ");
   }
 }
